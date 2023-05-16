edaa22c2dfeda9a1caaf74b19f1b2da71aaf0707
Parselinho <parselinho@gmail.com>
2023-05-16 21:38:46 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+
 
 function App() {
-  const [courses, setCourses] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/courses')
-      .then(response => {
-        setCourses(response.data);
-        console.log(response.data);
-      }) 
-      .catch(error => { 
-        console.log('Error fetching and parsing data', error);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>Courses:</h1>
-      <ul>
-        { courses.map(course => <li key={course.id}>{course.title}</li>) }
-      </ul>
-    </div>
-  )
 }
 
 export default App
