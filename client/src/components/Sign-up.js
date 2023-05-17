6410252ea27b091fd731f5bdbeabfba939beae0f
Parselinho <parselinho@gmail.com>
2023-05-17 21:19:44 +0300

    Update Sign-up.js

--- client/src/components/Sign-up.js
+++ client/src/components/Sign-up.js
@@ -1,28 +1,30 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import Head from './Head';
 
 const UserSignUp = () => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
+
     const history = useHistory();
 
-    const handleSignUp = async (e) => {
-        e.preventDefault();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
         try {
             const response = await axios.post('http://localhost:5000/api/users', {
                 firstName,
                 lastName,
-                emailAddress: email,
+                emailAddress,
                 password
             });
 
             if (response.status === 201) {
                 // user created successfully, now sign in the user
-                const encodedCredentials = window.btoa(`${email}:${password}`);
+                const encodedCredentials = window.btoa(`${emailAddress}:${password}`);
                 const authHeader = `Basic ${encodedCredentials}`;
 
                 const response = await axios.get('http://localhost:5000/api/users', {
@@ -38,36 +40,66 @@ const UserSignUp = () => {
                 }
             }
         } catch (error) {
-            console.error("Error signing up", error);
+            console.error("Error creating user", error);
         }
     };
 
-    const handleCancel = () => {
-        history.push('/'); // navigate to the default route
+    const handleCancel = (event) => {
+        event.preventDefault();
+        history.push('/courses'); // navigate to the list of courses
     };
 
     return (
-        <form onSubmit={handleSignUp}>
-            <label>
-                First Name:
-                <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} required />
-            </label>
-            <label>
-                Last Name:
-                <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} required />
-            </label>
-            <label>
-                Email:
-                <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
-            </label>
-            <button type="submit">Sign Up</button>
-            <button type="button" onClick={handleCancel}>Cancel</button>
-        </form>
-    );
+        <div>
+          <Head />
+            <main>
+                <div className="form--centered">
+                    <h2>Sign Up</h2>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="firstName">First Name</label>
+                        <input 
+                            id="firstName" 
+                            name="firstName" 
+                            type="text" 
+                            value={firstName} 
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
+
+                        <label htmlFor="lastName">Last Name</label>
+                        <input 
+                            id="lastName" 
+                            name="lastName" 
+                            type="text" 
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
+
+                        <label htmlFor="emailAddress">Email Address</label>
+                        <input 
+                            id="emailAddress" 
+                            name="emailAddress" 
+                            type="email" 
+                            value={emailAddress}
+                            onChange={(e) => setEmailAddress(e.target.value)}
+                        />
+
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            id="password" 
+                            name="password" 
+                            type="password" 
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+
+                        <button className="button" type="submit">Sign Up</button>
+                        <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
+                    </form>
+                    <p>Already have a user account? Click here to <a href="/sign-in">sign in</a>!</p>
+                </div>
+            </main>
+        </div>
+    )
 };
 
 export default UserSignUp;
