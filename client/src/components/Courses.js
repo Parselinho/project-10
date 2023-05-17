7963df5fb3aeb80bd7f2e4a18451b24c54ceed30
Parselinho <parselinho@gmail.com>
2023-05-17 21:19:33 +0300

    Update Courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,36 +1,47 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import Header from './Header';
+import Head from './Head';
 
 function Courses() {
     const [courses, setCourses] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-      axios.get('http://localhost:5000/api/courses')
-        .then(response => {
-          setCourses(response.data);
-          console.log(response.data);
-        }) 
-        .catch(error => { 
-          console.log('Error fetching and parsing data', error);
-        });
+        axios.get('http://localhost:5000/api/courses')
+            .then(response => {
+                setCourses(response.data);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                setError('Error fetching and parsing data');
+                setIsLoading(false);
+            });
     }, []);
-  
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+
+    if (error) {
+        return <div>{error}</div>
+    }
+
     return (
-      <div>
-      <Header />
-        <h1>Courses:</h1>
-        <ul>
-            {courses.map(course => (
-                <li key={course.id}>
-                    <Link to={`/courses/${course.id}`}>{course.title}</Link>
-                </li>
-            ))}
-        </ul>
-        <Link to="/courses/create">Create Course</Link>
-      </div>
+        <div>
+            <Head />
+            <h1>Courses:</h1>
+            <ul>
+                {courses.map(course => (
+                    <li key={course.id}>
+                        <Link to={`/courses/${course.id}`}>{course.title}</Link>
+                    </li>
+                ))}
+            </ul>
+            <Link to="/courses/create">Create Course</Link>
+        </div>
     )
 }
 
-export default Courses
+export default Courses;
