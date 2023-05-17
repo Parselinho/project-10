f03116808dd1596313aaa3e15fa7d91bf429f2c8
Parselinho <parselinho@gmail.com>
2023-05-17 21:19:49 +0300

    Create Update-course.js

---
+++ client/src/components/Update-course.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useHistory, Link } from 'react-router-dom';
+import axios from 'axios';
+import Head from './Head';
+
+const UpdateCourse = () => {
+    const { id } = useParams();
+    const history = useHistory();
+    const [course, setCourse] = useState({
+        title: '',
+        description: '',
+        estimatedTime: '',
+        materialsNeeded: ''
+    });
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/courses/${id}`)
+            .then(response => {
+                setCourse(response.data);
+            })
+            .catch(error => {
+                console.log('Error fetching and parsing data', error);
+            });
+    }, [id]);
+
+    const handleChange = (event) => {
+        setCourse({
+            ...course,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.put(`http://localhost:5000/api/courses/${id}`, course)
+            .then(() => {
+                history.push(`/courses/${id}`);
+            })
+            .catch(error => {
+                console.log('Error updating course', error);
+            });
+    }
+
+    return (
+        <div>
+            <Head />
+            <div className="wrap">
+                <h2>Update Course</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="main--flex">
+                        <div>
+                            <label htmlFor="courseTitle">Course Title</label>
+                            <input id="courseTitle" name="title" type="text" value={course.title} onChange={handleChange} />
+
+                            <p>By Joe Smith</p>
+
+                            <label htmlFor="courseDescription">Course Description</label>
+                            <textarea id="courseDescription" name="description" onChange={handleChange}>{course.description}</textarea>
+                        </div>
+                        <div>
+                            <label htmlFor="estimatedTime">Estimated Time</label>
+                            <input id="estimatedTime" name="estimatedTime" type="text" value={course.estimatedTime} onChange={handleChange} />
+
+                            <label htmlFor="materialsNeeded">Materials Needed</label>
+                            <textarea id="materialsNeeded" name="materialsNeeded" onChange={handleChange}>{course.materialsNeeded}</textarea>
+                        </div>
+                    </div>
+                    <button className="button" type="submit">Update Course</button>
+                    <Link className="button button-secondary" to={`/courses/${id}`}>Cancel</Link>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default UpdateCourse;
