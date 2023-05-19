c2c4648ad8ae6804b6b279383d036afc6d0df986
Parselinho <parselinho@gmail.com>
2023-05-19 18:17:26 +0300

    Update Course-details.js

--- client/src/components/Course-details.js
+++ client/src/components/Course-details.js
@@ -56,19 +56,55 @@ function CourseDetail() {
             });
     };
 
-    const materials = course.materialsNeeded.split('\n').map((item, index) => <li key={index}>{item}</li>);
+    const materials = course.materialsNeeded ? course.materialsNeeded.split('\n').map((item, index) => <li key={index}>{item}</li>) : [];
 
     return (
-        <div>
+        <>
             <Head />
-            <h1>{course.title}</h1>
-            <p>By {user.firstName} {user.lastName}</p>
-            <p>{course.description}</p>
-            <ul>{materials}</ul>
-            <button onClick={handleDelete}>Delete Course</button>
-            <Link to={`/courses/${course.id}/update`}>Update Course</Link>
-        </div>
-    )
+            <div id="root">
+            <div className="wrap">
+                <h1 className="header--logo"><Link to="/">Courses</Link></h1>
+                <nav>
+                    <ul className="header--signedin">
+                        <li>Welcome, {user && `${user.firstName} ${user.lastName}`}!</li>
+                        <li><Link to="/sign-out">Sign Out</Link></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <main>
+                <div className="actions--bar">
+                    <div className="wrap">
+                        <Link className="button" to={`/courses/${course.id}/update`}>Update Course</Link>
+                        <button className="button" onClick={handleDelete}>Delete Course</button>
+                        <Link className="button button-secondary" to="/">Return to List</Link>
+                    </div>
+                </div>
+
+                <div className="wrap">
+                    <h2>Course Detail</h2>
+                    <form>
+                        <div className="main--flex">
+                            <div>
+                                <h3 className="course--detail--title">Course</h3>
+                                <h4 className="course--name">{course.title}</h4>
+                                <p>By {user && `${user.firstName} ${user.lastName}`}</p>
+                                <p>{course.description}</p>
+                            </div>
+                            <div>
+                                <h3 className="course--detail--title">Estimated Time</h3>
+                                <p>{course.estimatedTime}</p>
+
+                                <h3 className="course--detail--title">Materials Needed</h3>
+                                <ul className="course--detail--list">{materials}</ul>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </main>
+            </div>
+        </>
+    );
 }
 
 export default CourseDetail;
