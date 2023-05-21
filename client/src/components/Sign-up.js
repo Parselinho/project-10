50e09c7628054f13012ed4aa96db6fce98e689af
Parselinho <parselinho@gmail.com>
2023-05-21 10:49:24 +0300

    Update Sign-up.js

--- client/src/components/Sign-up.js
+++ client/src/components/Sign-up.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Head from './Head';
+import { AuthContext } from './context/AuthContext';
 
 const UserSignUp = () => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
+
+    const { setUser } = useContext(AuthContext);
 
     const navigate = useNavigate();
 
@@ -35,8 +38,10 @@ const UserSignUp = () => {
 
                 if (response.status === 200) {
                     // user signed in successfully
+                    // Set the user in context
+                    setUser(response.data);
                     // navigate to courses
-                    navigate.push('/courses');
+                    navigate('/courses');
                 }
             }
         } catch (error) {
@@ -46,60 +51,75 @@ const UserSignUp = () => {
 
     const handleCancel = (event) => {
         event.preventDefault();
-        navigate.push('/courses'); // navigate to the list of courses
+        navigate('/courses'); // navigate to the list of courses
     };
 
     return (
-        <div>
-          <Head />
+        <>
+        <Head />
+        <div id="root">
+            <header>
+                <div className="wrap header--flex">
+                    <h1 className="header--logo">
+                        <Link to="/courses">Courses</Link>
+                    </h1>
+                    <nav>
+                        <ul className="header--signedout">
+                            <li>
+                                <Link to="/signup">Sign Up</Link>
+                            </li>
+                            <li>
+                                <Link to="/signin">Sign In</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
             <main>
                 <div className="form--centered">
                     <h2>Sign Up</h2>
                     <form onSubmit={handleSubmit}>
                         <label htmlFor="firstName">First Name</label>
-                        <input 
-                            id="firstName" 
-                            name="firstName" 
-                            type="text" 
-                            value={firstName} 
-                            onChange={(e) => setFirstName(e.target.value)}
-                        />
+                        <input
+                            id="firstName"
+                            name="firstName"
+                            type="text"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)} />
 
                         <label htmlFor="lastName">Last Name</label>
-                        <input 
-                            id="lastName" 
-                            name="lastName" 
-                            type="text" 
+                        <input
+                            id="lastName"
+                            name="lastName"
+                            type="text"
                             value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                        />
+                            onChange={(e) => setLastName(e.target.value)} />
 
                         <label htmlFor="emailAddress">Email Address</label>
-                        <input 
-                            id="emailAddress" 
-                            name="emailAddress" 
-                            type="email" 
+                        <input
+                            id="emailAddress"
+                            name="emailAddress"
+                            type="email"
                             value={emailAddress}
-                            onChange={(e) => setEmailAddress(e.target.value)}
-                        />
+                            onChange={(e) => setEmailAddress(e.target.value)} />
 
                         <label htmlFor="password">Password</label>
-                        <input 
-                            id="password" 
-                            name="password" 
-                            type="password" 
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
                             value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
+                            onChange={(e) => setPassword(e.target.value)} />
 
                         <button className="button" type="submit">Sign Up</button>
                         <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
                     </form>
-                    <p>Already have a user account? Click here to <a href="/sign-in">sign in</a>!</p>
+                    <p>Already have a user account? <Link to="/signin">Click here</Link> to sign in!</p>
                 </div>
             </main>
         </div>
-    )
+        </>
+    );
 };
 
 export default UserSignUp;
