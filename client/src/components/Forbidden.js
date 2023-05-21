81e7f00f407b98cef94c7c3caa099fe962759dc2
Parselinho <parselinho@gmail.com>
2023-05-21 10:49:17 +0300

    Create Forbidden.js

---
+++ client/src/components/Forbidden.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Head from './Head';
+
+
+const ForbiddenPage = () => {
+    return (
+        <><Head /><div id="root">
+            <header>
+                <div className="wrap header--flex">
+                    <h1 className="header--logo">
+                        <Link to="/courses">Courses</Link>
+                    </h1>
+                    <nav>
+                        <ul className="header--signedin">
+                            <li>Welcome, Joe Smith!</li>
+                            <li>
+                                <Link to="/sign-out">Sign Out</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <main>
+                <div className="wrap">
+                    <h2>Forbidden</h2>
+                    <p>Oh oh! You can't access this page.</p>
+                </div>
+            </main>
+        </div></>
+    );
+};
+
+export default ForbiddenPage;
