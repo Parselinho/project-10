5e8697ad1c56b1201c59c8e3fc9a59d259fc6af9
Parselinho <parselinho@gmail.com>
2023-05-21 21:32:31 +0300

    Update Sign-up.js

--- client/src/components/Sign-up.js
+++ client/src/components/Sign-up.js
@@ -1,16 +1,13 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import Head from './Head';
-import { AuthContext } from './context/AuthContext';
 
-const UserSignUp = () => {
+const UserSignUp = ({ onSignUp }) => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
-
-    const { setUser } = useContext(AuthContext);
 
     const navigate = useNavigate();
 
@@ -26,23 +23,8 @@ const UserSignUp = () => {
             });
 
             if (response.status === 201) {
-                // user created successfully, now sign in the user
-                const encodedCredentials = window.btoa(`${emailAddress}:${password}`);
-                const authHeader = `Basic ${encodedCredentials}`;
-
-                const response = await axios.get('http://localhost:5000/api/users', {
-                    headers: {
-                        'Authorization': authHeader
-                    }
-                });
-
-                if (response.status === 200) {
-                    // user signed in successfully
-                    // Set the user in context
-                    setUser(response.data);
-                    // navigate to courses
-                    navigate('/courses');
-                }
+                onSignUp(emailAddress, password);
+                navigate('/courses');
             }
         } catch (error) {
             console.error("Error creating user", error);
@@ -51,72 +33,29 @@ const UserSignUp = () => {
 
     const handleCancel = (event) => {
         event.preventDefault();
-        navigate('/courses'); // navigate to the list of courses
+        navigate('/courses');
     };
 
     return (
         <>
         <Head />
-        <div id="root">
-            <header>
-                <div className="wrap header--flex">
-                    <h1 className="header--logo">
-                        <Link to="/courses">Courses</Link>
-                    </h1>
-                    <nav>
-                        <ul className="header--signedout">
-                            <li>
-                                <Link to="/signup">Sign Up</Link>
-                            </li>
-                            <li>
-                                <Link to="/signin">Sign In</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
-            <main>
-                <div className="form--centered">
-                    <h2>Sign Up</h2>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="firstName">First Name</label>
-                        <input
-                            id="firstName"
-                            name="firstName"
-                            type="text"
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)} />
-
-                        <label htmlFor="lastName">Last Name</label>
-                        <input
-                            id="lastName"
-                            name="lastName"
-                            type="text"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)} />
-
-                        <label htmlFor="emailAddress">Email Address</label>
-                        <input
-                            id="emailAddress"
-                            name="emailAddress"
-                            type="email"
-                            value={emailAddress}
-                            onChange={(e) => setEmailAddress(e.target.value)} />
-
-                        <label htmlFor="password">Password</label>
-                        <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)} />
-
-                        <button className="button" type="submit">Sign Up</button>
-                        <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
-                    </form>
-                    <p>Already have a user account? <Link to="/signin">Click here</Link> to sign in!</p>
-                </div>
-            </main>
+        <div className="form--centered">
+            <h2>Sign Up</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="firstName">First Name</label>
+                <input id="firstName" name="firstName" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                <label htmlFor="lastName">Last Name</label>
+                <input id="lastName" name="lastName" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                <label htmlFor="emailAddress">Email Address</label>
+                <input id="emailAddress" name="emailAddress" type="email" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} />
+                <label htmlFor="password">Password</label>
+                <input id="password" name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button className="button" type="submit">Sign Up</button>
+                <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
+            </form>
+            <p>
+                Already have a user account? <Link to="/signin">Click here</Link> to sign in!
+            </p>
         </div>
         </>
     );
