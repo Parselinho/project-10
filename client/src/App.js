9f216ffb7aca1c4ced710faae40ccf11339e8710
Parselinho <parselinho@gmail.com>
2023-05-21 21:32:15 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,46 @@
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Header from "./components/Header";
 import Courses from "./components/Courses";
 import CourseDetail from "./components/Course-details";
 import CreateCourse from "./components/Create-course";
 import UpdateCourse from "./components/Update-course";
 import UserSignIn from "./components/Sign-in";
 import UserSignUp from "./components/Sign-up";
+import UserSignOut from "./components/SignOut";
+import { AuthContext } from "./components/context/AuthContext";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const signIn = async (email, password) => {
+    // handle sign in logic and setUser
+  };
+
+  const signOut = () => {
+    setUser(null);
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/courses/*" element={
-          <Routes>
-            <Route path="/" element={<Courses />} />
-            <Route path="/:id" element={<CourseDetail />} />
-            <Route path="/create" element={<CreateCourse />} />
-            <Route path="/:id/update" element={<UpdateCourse />} />
-          </Routes>
-        } />
-        <Route path="/signin" element={<UserSignIn />} />
-        <Route path="/signup" element={<UserSignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={{ user, signIn, signOut }}>
+      <BrowserRouter>
+        <Header user={user} onSignOut={signOut} />
+        <Routes>
+          <Route path="/courses/*" element={
+            <Routes>
+              <Route path="/" element={<Courses />} />
+              <Route path="/:id" element={<CourseDetail />} />
+              <Route path="/create" element={<CreateCourse />} />
+              <Route path="/:id/update" element={<UpdateCourse />} />
+            </Routes>
+          } />
+          <Route path="/signin" element={<UserSignIn onSignIn={signIn} />} />
+          <Route path="/signup" element={<UserSignUp onSignUp={signIn} />} />
+          <Route path="/signout" element={<UserSignOut onSignOut={signOut} />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
