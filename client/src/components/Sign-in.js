bf6aefae18115929d838aa747210cae2c89721b8
Parselinho <parselinho@gmail.com>
2023-05-22 13:10:25 +0300

    Update Sign-in.js

--- client/src/components/Sign-in.js
+++ client/src/components/Sign-in.js
@@ -1,59 +1,60 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import Head from './Head';
+import { AuthContext } from './context/AuthContext';
 
-const UserSignIn = ({ onSignIn }) => {
+const UserSignIn = () => {
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
 
+    const { signIn } = useContext(AuthContext);
     const navigate = useNavigate();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-
-        try {
-            const encodedCredentials = window.btoa(`${emailAddress}:${password}`);
-            const authHeader = `Basic ${encodedCredentials}`;
-
-            const response = await axios.get('http://localhost:5000/api/users', {
-                headers: {
-                    'Authorization': authHeader
-                }
-            });
-
-            if (response.status === 200) {
-                onSignIn(emailAddress, password);
-                navigate('/courses');
-            }
-        } catch (error) {
-            console.error("Error signing in", error);
-        }
-    };
-
-    const handleCancel = (event) => {
-        event.preventDefault();
+        // sign-in the user using the signIn function in the AuthContext
+        await signIn(emailAddress, password);
+        // redirect to the courses page
         navigate('/courses');
     };
 
     return (
-        <>
-        <Head />
-        <div className="form--centered">
-            <h2>Sign In</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="emailAddress">Email Address</label>
-                <input id="emailAddress" name="emailAddress" type="email" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} />
-                <label htmlFor="password">Password</label>
-                <input id="password" name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button className="button" type="submit">Sign In</button>
-                <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
-            </form>
-            <p>
-                Don't have a user account? <Link to="/signup">Click here</Link> to sign up!
-            </p>
+        <div className="bounds">
+            <div className="grid-33 centered signin">
+                <h1>Sign In</h1>
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <input 
+                                id="emailAddress" 
+                                name="emailAddress" 
+                                type="text"
+                                className="" 
+                                placeholder="Email Address" 
+                                value={emailAddress}
+                                onChange={(e) => setEmailAddress(e.target.value)} 
+                            />
+                        </div>
+                        <div>
+                            <input 
+                                id="password" 
+                                name="password" 
+                                type="password"
+                                className="" 
+                                placeholder="Password" 
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} 
+                            />
+                        </div>
+                        <div className="grid-100 pad-bottom">
+                            <button className="button" type="submit">Sign In</button>
+                            <Link to="/" className="button button-secondary">Cancel</Link>
+                        </div>
+                    </form>
+                </div>
+                <p>&nbsp;</p>
+                <p>Don't have a user account? <Link to="/signup">Click here</Link> to sign up!</p>
+            </div>
         </div>
-        </>
     );
 };
 
