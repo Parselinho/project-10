c71e8363a483a0a9888ae088fe9e5950f7d7c940
Parselinho <parselinho@gmail.com>
2023-05-22 16:11:51 +0300

    Update AuthContext.js

--- client/src/components/context/AuthContext.js
+++ client/src/components/context/AuthContext.js
@@ -1,13 +1,33 @@
 import React, { createContext, useState } from 'react';
+import axios from 'axios'; 
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [authenticatedUser, setAuthenticatedUser] = useState(null);
 
-  const signIn = (user) => {
-    setAuthenticatedUser(user);
+  const signIn = async (emailAddress, password) => {
+    try {
+      // Encode the credentials
+      const encodedCredentials = btoa(`${emailAddress}:${password}`);
+      
+      // Make a GET request to the /users endpoint with the credentials in the Authorization header
+      const response = await axios.get('/api/users', {
+        headers: {
+          'Authorization': `Basic ${encodedCredentials}`
+        }
+      });
+      
+      // If the response status is 200, set the user to be the authenticated user
+      if (response.status === 200) {
+        setAuthenticatedUser(response.data.user); // Make sure that user data is correctly referenced.
+      }
+    } catch (error) {
+      console.error("Error signing in", error);
+      throw error; // Re-throw the error so it can be caught and handled by the calling code
+    }
   };
+  
 
   const signOut = () => {
     setAuthenticatedUser(null);
