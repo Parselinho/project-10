f6b2f0d4f2ea59544923756c47ad05cbce309af4
Parselinho <parselinho@gmail.com>
2023-05-22 13:10:23 +0300

    Update Sign-up.js

--- client/src/components/Sign-up.js
+++ client/src/components/Sign-up.js
@@ -1,29 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-import Head from './Head';
+import { useForm } from "react-hook-form";
 
 const UserSignUp = ({ onSignUp }) => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [emailAddress, setEmailAddress] = useState('');
-    const [password, setPassword] = useState('');
-
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const navigate = useNavigate();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
+    const onSubmit = async (data) => {
         try {
-            const response = await axios.post('http://localhost:5000/api/users', {
-                firstName,
-                lastName,
-                emailAddress,
-                password
-            });
+            const response = await axios.post('http://localhost:5000/api/users', data);
 
             if (response.status === 201) {
-                onSignUp(emailAddress, password);
+                onSignUp(data.emailAddress, data.password);
                 navigate('/courses');
             }
         } catch (error) {
@@ -38,18 +27,25 @@ const UserSignUp = ({ onSignUp }) => {
 
     return (
         <>
-        <Head />
         <div className="form--centered">
             <h2>Sign Up</h2>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="firstName">First Name</label>
-                <input id="firstName" name="firstName" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                <input {...register("firstName", { required: true })} id="firstName" name="firstName" type="text" />
+                {errors.firstName && <span>This field is required</span>}
+
                 <label htmlFor="lastName">Last Name</label>
-                <input id="lastName" name="lastName" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                <input {...register("lastName", { required: true })} id="lastName" name="lastName" type="text" />
+                {errors.lastName && <span>This field is required</span>}
+
                 <label htmlFor="emailAddress">Email Address</label>
-                <input id="emailAddress" name="emailAddress" type="email" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} />
+                <input {...register("emailAddress", { required: true })} id="emailAddress" name="emailAddress" type="email" />
+                {errors.emailAddress && <span>This field is required</span>}
+
                 <label htmlFor="password">Password</label>
-                <input id="password" name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input {...register("password", { required: true })} id="password" name="password" type="password" />
+                {errors.password && <span>This field is required</span>}
+
                 <button className="button" type="submit">Sign Up</button>
                 <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
             </form>
