7a415b1e6872b8e85c62230fd7654f8f2e33cc50
Parselinho <parselinho@gmail.com>
2023-05-24 09:35:16 +0300

    Update Create-course.js

--- client/src/components/Create-course.js
+++ client/src/components/Create-course.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
+import { AuthContext } from './context/AuthContext';
 
 const CreateCourse = () => {
     const [title, setTitle] = useState('');
@@ -10,16 +10,12 @@ const CreateCourse = () => {
     const [materialsNeeded, setMaterialsNeeded] = useState('');
     const [errors, setErrors] = useState([]);
     const navigate = useNavigate();
+    const { authenticatedUser } = useContext(AuthContext);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        let errorMessages = [];
-        if (title === '') errorMessages.push("Please provide a value for 'Title'");
-        if (description === '') errorMessages.push("Please provide a value for 'Description'");
-        setErrors(errorMessages);
-
-        if (errorMessages.length > 0) return;
+        const encodedCredentials = btoa(`${authenticatedUser.email}:${authenticatedUser.password}`);
 
         try {
             const response = await axios.post('http://localhost:5000/api/courses', {
@@ -27,6 +23,10 @@ const CreateCourse = () => {
                 description,
                 estimatedTime,
                 materialsNeeded
+            }, {
+                headers: {
+                    'Authorization': `Basic ${encodedCredentials}`
+                }
             });
 
             if (response.status === 201) {
@@ -34,89 +34,77 @@ const CreateCourse = () => {
             }
         } catch (error) {
             console.error("Error creating course", error);
+            if (error.response && error.response.data.errors) {
+                setErrors(error.response.data.errors);
+            }
         }
     };
 
     const handleCancel = (event) => {
         event.preventDefault();
-        navigate('/courses'); // navigate to the list of courses
+        navigate('/courses'); 
     };
 
     return (
-            <>
+        <>
             <div>
-            {/* <header>
-                <div className="wrap header--flex">
-                    <h1 className="header--logo">
-                       <Link to="/courses">Courses</Link> 
-                    </h1>
-                    <nav>
-                        <ul className="header-signedin">
-                            <li>Welcome Joe Smith!</li>
-                            <li>
-                                <Link to="/signout">Sign Out</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header> */}
-
-            <main>
-                <div className="wrap">
-                    <h2>Create Course</h2>
-                    {errors.length > 0 && (
-                        <div className="validation--errors">
-                            <h3>Validation Errors</h3>
-                            <ul>
-                                {errors.map((error, i) => <li key={i}>{error}</li>)}
-                            </ul>
-                        </div>
-                    )}
-                    <form onSubmit={handleSubmit}>
-                        <div className="main--flex">
-                            <div>
-                                <label htmlFor="courseTitle">Course Title</label>
-                                <input
-                                    id="courseTitle"
-                                    name="courseTitle"
-                                    type="text"
-                                    value={title}
-                                    onChange={(e) => setTitle(e.target.value)} />
-
-                                <p>By Joe Smith</p>
-
-                                <label htmlFor="courseDescription">Course Description</label>
-                                <textarea
-                                    id="courseDescription"
-                                    name="courseDescription"
-                                    value={description}
-                                    onChange={(e) => setDescription(e.target.value)}
-                                ></textarea>
+                <main>
+                    <div className="wrap">
+                        <h2>Create Course</h2>
+                        {errors.length > 0 && (
+                            <div className="validation--errors">
+                                <h3>Validation Errors</h3>
+                                <ul>
+                                    {errors.map((error, i) => <li key={i}>{error}</li>)}
+                                </ul>
                             </div>
-                            <div>
-                                <label htmlFor="estimatedTime">Estimated Time</label>
-                                <input
-                                    id="estimatedTime"
-                                    name="estimatedTime"
-                                    type="text"
-                                    value={estimatedTime}
-                                    onChange={(e) => setEstimatedTime(e.target.value)} />
+                        )}
+                        <form onSubmit={handleSubmit}>
+                            <div className="main--flex">
+                                <div>
+                                    <label htmlFor="courseTitle">Course Title</label>
+                                    <input
+                                        id="courseTitle"
+                                        name="courseTitle"
+                                        type="text"
+                                        value={title}
+                                        onChange={(e) => setTitle(e.target.value)} />
 
-                                <label htmlFor="materialsNeeded">Materials Needed</label>
-                                <textarea
-                                    id="materialsNeeded"
-                                    name="materialsNeeded"
-                                    value={materialsNeeded}
-                                    onChange={(e) => setMaterialsNeeded(e.target.value)}
-                                ></textarea>
+                                    <p>By Joe Smith</p>
+
+                                    <label htmlFor="courseDescription">Course Description</label>
+                                    <textarea
+                                        id="courseDescription"
+                                        name="courseDescription"
+                                        value={description}
+                                        onChange={(e) => setDescription(e.target.value)}
+                                    ></textarea>
+                                </div>
+                                <div>
+                                    <label htmlFor="estimatedTime">Estimated Time</label>
+                                    <input
+                                        id="estimatedTime"
+                                        name="estimatedTime"
+                                        type="text"
+                                        value={estimatedTime}
+                                        onChange={(e) => setEstimatedTime(e.target.value)} />
+
+                                    <label htmlFor="materialsNeeded">Materials Needed</label>
+                                    <textarea
+                                        id="materialsNeeded"
+                                        name="materialsNeeded"
+                                        value={materialsNeeded}
+                                        onChange={(e) => setMaterialsNeeded(e.target.value)}
+                                    ></textarea>
+                                </div>
                             </div>
-                        </div>
-                        <button className="button" type="submit">Create Course</button>
-                        <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
-                    </form>
-                </div>
-            </main>
-        </div></>
+                            <button className="button" type="submit">Create Course</button>
+                            <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
+                        </form>
+                    </div>
+                </main>
+            </div>
+        </>
     )
 };
 
