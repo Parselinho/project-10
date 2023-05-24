eec391c31256d0f474cf9564c6f79d9a8887810d
Parselinho <parselinho@gmail.com>
2023-05-24 09:35:13 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,28 +8,25 @@ import UpdateCourse from "./components/Update-course";
 import UserSignIn from "./components/Sign-in";
 import UserSignUp from "./components/Sign-up";
 import UserSignOut from "./components/SignOut";
-import { AuthProvider } from "./components/context/AuthContext";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/courses/*" element={
-            <Routes>
-              <Route path="/" element={<Courses />} />
-              <Route path="/:id" element={<CourseDetail />} />
-              <Route path="/create" element={<CreateCourse />} />
-              <Route path="/:id/update" element={<UpdateCourse />} />
-            </Routes>
-          } />
-          <Route path="/signin" element={<UserSignIn />} />
-          <Route path="/signup" element={<UserSignUp />} />
-          <Route path="/signout" element={<UserSignOut />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/courses/*" element={
+          <Routes>
+            <Route path="/" element={<Courses />} />
+            <Route path="/:id" element={<CourseDetail />} />
+            <Route path="/create" element={<CreateCourse />} />
+            <Route path="/:id/update" element={<UpdateCourse />} />
+          </Routes>
+        } />
+        <Route path="/signin" element={<UserSignIn />} />
+        <Route path="/signup" element={<UserSignUp />} />
+        <Route path="/signout" element={<UserSignOut />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
