ce68ffcf291807f23570ae332f5ab8d48b9bbfda
Parselinho <parselinho@gmail.com>
2023-05-28 13:42:34 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
-import { useEffect } from 'react';
+import { useEffect, useContext } from 'react';
 
+import { AuthProvider, AuthContext } from "./components/context/AuthContext";
 import Header from "./components/Header";
 import Courses from "./components/Courses";
 import CourseDetail from "./components/Course-details";
@@ -9,8 +10,7 @@ import UpdateCourse from "./components/Update-course";
 import UserSignIn from "./components/Sign-in";
 import UserSignUp from "./components/Sign-up";
 import UserSignOut from "./components/SignOut";
-import PrivateRoute from './components/PrivateRoute';
-
+import PrivateRoute from "./components/PrivateRoute";
 
 function RedirectToCourses() {
   const navigate = useNavigate();
@@ -23,24 +23,36 @@ function RedirectToCourses() {
 }
 
 function App() {
+  const { signIn } = useContext(AuthContext);
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<RedirectToCourses />} />
-        <Route path="/courses/*" element={
-          <Routes>
-            <Route path="/" element={<Courses />} />
-            <Route path="/:id" element={<CourseDetail />} />
-            <PrivateRoute path="/create" element={<CreateCourse />} />
-            <PrivateRoute path="/:id/update" element={<UpdateCourse />} />
-          </Routes>
-        } />
-        <Route path="/signin" element={<UserSignIn />} />
-        <Route path="/signup" element={<UserSignUp />} />
-        <Route path="/signout" element={<UserSignOut />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<RedirectToCourses />} />
+          <Route path="/courses/*" element={
+            <Routes>
+              <Route path="/" element={<Courses />} />
+              <Route path="/:id" element={<CourseDetail />} />
+              <Route path="/create" element={
+                <PrivateRoute>
+                  <CreateCourse />
+                </PrivateRoute>
+              } />
+              <Route path="/:id/update" element={
+                <PrivateRoute>
+                  <UpdateCourse />
+                </PrivateRoute>
+              } />
+            </Routes>
+          } />
+          <Route path="/signin" element={<UserSignIn />} />
+          <Route path="/signup" element={<UserSignUp onSignUp={signIn} />} />
+          <Route path="/signout" element={<UserSignOut />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
