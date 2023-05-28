5d709ca8d2287b0cf80951e07e550c1f9643958b
Parselinho <parselinho@gmail.com>
2023-05-28 11:56:14 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
 
 import Header from "./components/Header";
 import Courses from "./components/Courses";
@@ -8,18 +9,31 @@ import UpdateCourse from "./components/Update-course";
 import UserSignIn from "./components/Sign-in";
 import UserSignUp from "./components/Sign-up";
 import UserSignOut from "./components/SignOut";
+import PrivateRoute from './components/PrivateRoute';
+
+
+function RedirectToCourses() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/courses');
+  }, [navigate]);
+
+  return null;
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path="/" element={<RedirectToCourses />} />
         <Route path="/courses/*" element={
           <Routes>
             <Route path="/" element={<Courses />} />
             <Route path="/:id" element={<CourseDetail />} />
-            <Route path="/create" element={<CreateCourse />} />
-            <Route path="/:id/update" element={<UpdateCourse />} />
+            <PrivateRoute path="/create" element={<CreateCourse />} />
+            <PrivateRoute path="/:id/update" element={<UpdateCourse />} />
           </Routes>
         } />
         <Route path="/signin" element={<UserSignIn />} />
