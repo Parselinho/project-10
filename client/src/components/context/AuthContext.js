1bacc2d535482bf7f1d4dc26e3a354d6adeab399
Parselinho <parselinho@gmail.com>
2023-05-29 11:45:05 +0300

    Update AuthContext.js

--- client/src/components/context/AuthContext.js
+++ client/src/components/context/AuthContext.js
@@ -1,12 +1,15 @@
 import { createContext, useState } from 'react';
+// import { useNavigate } from 'react-router-dom'; // added for redirecting after sign-in
 import axios from 'axios'; 
 
 export const AuthContext = createContext();
 
+
 export const AuthProvider = ({ children }) => {
   const [authenticatedUser, setAuthenticatedUser] = useState(null);
+  // const navigate = useNavigate();
 
-  const signIn = async (emailAddress, password, navigate) => {
+  const signIn = async (emailAddress, password) => {
     try {
       const encodedCredentials = btoa(`${emailAddress}:${password}`);
       const response = await axios.get('http://localhost:5000/api/users', {
@@ -20,13 +23,16 @@ export const AuthProvider = ({ children }) => {
         setAuthenticatedUser(user);
         console.log("Response Data: ", response.data);
   
-        navigate('/courses'); // navigate to the courses page after successful sign-in
-        return user;
+        // Do not navigate here. Just return a boolean indicating success.
+        return true;
       }
     } catch (error) {
       console.error("Error signing in", error);
       throw error; 
     }
+
+    // Return false in case of failure
+    return false;
   };
 
   const signOut = () => {
