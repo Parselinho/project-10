3a45e7f57a67f6ff597f5bd0654ceead3ac20a26
Parselinho <parselinho@gmail.com>
2023-05-29 18:02:53 +0300

    Update PrivateRoute.js

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,19 +1,21 @@
 import { useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext';
 
 // PrivateRoute component
 const PrivateRoute = ({ children }) => {
-  // Access authenticatedUser from AuthContext
-  const { authenticatedUser } = useContext(AuthContext);
+  // Access authenticatedUser and setLastVisitedPage from AuthContext
+  const { authenticatedUser, setLastVisitedPage } = useContext(AuthContext);
   const navigate = useNavigate();
+  const location = useLocation(); // useLocation is a hook provided by react-router-dom to get the current location object
 
   // Check if user is authenticated on component mount
   useEffect(() => {
     if (!authenticatedUser) {
-      navigate('/signin'); // Redirect to sign in page if not authenticated
+      setLastVisitedPage(location.pathname);
+      navigate('/signin', { state: { from: location.pathname } });
     }
-  }, [authenticatedUser, navigate]);
+  }, [authenticatedUser, navigate, setLastVisitedPage, location]);
 
   // Render the children components if user is authenticated, otherwise render null
   return authenticatedUser ? children : null;
