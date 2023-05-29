f39605bd3d2624afb25b4ada8ed434168b04b6d2
Parselinho <parselinho@gmail.com>
2023-05-29 12:24:38 +0300

    Update Forbidden.js

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 
 const ForbiddenPage = () => {
     return (
         <><div>
-            {/* <header>
-                <div className="wrap header--flex">
-                    <h1 className="header--logo">
-                        <Link to="/courses">Courses</Link>
-                    </h1>
-                    <nav>
-                        <ul className="header--signedin">
-                            <li>Welcome, Joe Smith!</li>
-                            <li>
-                                <Link to="/sign-out">Sign Out</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header> */}
             <main>
                 <div className="wrap">
                     <h2>Forbidden</h2>
