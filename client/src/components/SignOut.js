aeadd0b26817b9418cb3cc4abca9d0afedb4f07f
Parselinho <parselinho@gmail.com>
2023-05-29 13:44:54 +0300

    Update SignOut.js

--- client/src/components/SignOut.js
+++ client/src/components/SignOut.js
@@ -1,14 +1,17 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+// UserSignOut component
 const UserSignOut = ({ onSignOut }) => {
     const navigate = useNavigate();
 
+    // Perform sign out and redirect to courses list
     useEffect(() => {
-        onSignOut();
-        navigate('/courses');
+        onSignOut(); // Call the onSignOut function passed as a prop
+        navigate('/courses'); // Redirect to courses list
     }, [onSignOut, navigate]);
 
+    // Render nothing (null) since this component is only used for side effects
     return null;
 };
 
