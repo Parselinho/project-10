f2727315d114a6d613c529a7e93c836de71fc3c8
Parselinho <parselinho@gmail.com>
2023-05-29 13:44:49 +0300

    Update PrivateRoute.js

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,16 +2,20 @@ import { useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext';
 
+// PrivateRoute component
 const PrivateRoute = ({ children }) => {
+  // Access authenticatedUser from AuthContext
   const { authenticatedUser } = useContext(AuthContext);
   const navigate = useNavigate();
 
+  // Check if user is authenticated on component mount
   useEffect(() => {
     if (!authenticatedUser) {
-      navigate('/signin');
+      navigate('/signin'); // Redirect to sign in page if not authenticated
     }
   }, [authenticatedUser, navigate]);
 
+  // Render the children components if user is authenticated, otherwise render null
   return authenticatedUser ? children : null;
 };
 
