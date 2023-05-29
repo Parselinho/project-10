60fbafe51c8ae095bf20e18144dd23d339062ace
Parselinho <parselinho@gmail.com>
2023-05-29 13:20:40 +0300

    Update AuthContext.js

--- client/src/components/context/AuthContext.js
+++ client/src/components/context/AuthContext.js
@@ -1,44 +1,49 @@
 import { createContext, useState } from 'react';
-// import { useNavigate } from 'react-router-dom'; // added for redirecting after sign-in
-import axios from 'axios'; 
+import axios from 'axios';
 
+// Create the authentication context
 export const AuthContext = createContext();
 
-
+// Authentication provider component
 export const AuthProvider = ({ children }) => {
+  // State to store the authenticated user
   const [authenticatedUser, setAuthenticatedUser] = useState(null);
-  // const navigate = useNavigate();
 
+  // Function to sign in a user
   const signIn = async (emailAddress, password) => {
     try {
+      // Encode the user credentials
       const encodedCredentials = btoa(`${emailAddress}:${password}`);
+      // Send a GET request to the API with the encoded credentials
       const response = await axios.get('http://localhost:5000/api/users', {
         headers: {
           'Authorization': `Basic ${encodedCredentials}`
         }
       });
-  
+
+      // If the response is successful
       if (response.status === 200) {
+        // Create a user object with the response data
         const user = { ...response.data, password, emailAddress };
+        // Set the authenticated user in the state
         setAuthenticatedUser(user);
-        console.log("Response Data: ", response.data);
-  
-        // Do not navigate here. Just return a boolean indicating success.
-        return true;
+
+        return true; // Return true to indicate successful sign-in
       }
     } catch (error) {
       console.error("Error signing in", error);
-      throw error; 
+      throw error; // Throw the error for error handling
     }
 
-    // Return false in case of failure
-    return false;
+    return false; // Return false if sign-in is unsuccessful
   };
 
+  // Function to sign out the user
   const signOut = () => {
-    setAuthenticatedUser(null);
+    setAuthenticatedUser(null); // Clear the authenticated user from the state
   };
 
+  // Render the authentication provider with the provided children components
   return (
     <AuthContext.Provider value={{ authenticatedUser, setAuthenticatedUser, signIn, signOut }}>
       {children}
