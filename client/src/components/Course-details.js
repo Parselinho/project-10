bfa61ee9a130b989798ecfb2626b954c239f5cd4
Parselinho <parselinho@gmail.com>
2023-05-29 12:24:34 +0300

    Update Course-details.js

--- client/src/components/Course-details.js
+++ client/src/components/Course-details.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState, useContext } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { AuthContext } from './context/AuthContext';
-// import Head from './Head';
+import ReactMarkdown from 'react-markdown';
+
 
 function CourseDetail() {
     const [course, setCourse] = useState(null);
@@ -16,13 +17,13 @@ function CourseDetail() {
 
     useEffect(() => {
         const source = axios.CancelToken.source();
-    
+
         axios.get(`http://localhost:5000/api/courses/${id}`, { cancelToken: source.token })
             .then(response => {
                 setCourse(response.data);
                 return axios.get(`http://localhost:5000/api/users/${response.data.userId}`,
-                { cancelToken: source.token }
-              );
+                    { cancelToken: source.token }
+                );
             })
             .then(response => {
                 setUser(response.data);
@@ -34,11 +35,11 @@ function CourseDetail() {
                     setIsLoading(false);
                 }
             });
-    
+
         return () => {
             source.cancel();
         };
-    }, [id]);    
+    }, [id]);
 
     if (isLoading) {
         return <div>Loading...</div>
@@ -56,61 +57,67 @@ function CourseDetail() {
                     'Authorization': `Basic ${encodedCredentials}`
                 }
             })
-            .then(() => {
-                navigate('/courses');
-            })
-            .catch(error => {
-                console.log('Error deleting course:', error);
-                setError('There was a problem deleting the course. Please try again.');
-            });
+                .then(() => {
+                    navigate('/courses');
+                })
+                .catch(error => {
+                    console.log('Error deleting course:', error);
+                    setError('There was a problem deleting the course. Please try again.');
+                });
         } else {
             // handle the case when authenticatedUser is null.
             navigate('/signin');
         }
     };
-    
 
-    const materials = course.materialsNeeded && course.materialsNeeded.trim() 
-    ? course.materialsNeeded.split('\n').filter(item => item.trim() !== '').map((item, index) => <li key={index}>{item}</li>) 
-    : [];
-  
+
+    const materials = course.materialsNeeded && course.materialsNeeded.trim()
+        ? course.materialsNeeded.split('\n').filter(item => item.trim() !== '').map((item, index) => <li key={index}>{item}</li>)
+        : [];
+
     return (
         <>
             <div>
-            <main>
-                <div className="actions--bar">
-                    <div className="wrap">
-                        <Link className="button" to={`/courses/${course.id}/update`}>Update Course</Link>
-                        <button className="button" onClick={handleDelete}>Delete Course</button>
-                        <Link className="button button-secondary" to="/courses">Return to List</Link>
-                    </div>
-                </div>
-
-                <div className="wrap">
-                    <h2>Course Detail</h2>
-                    <form>
-                        <div className="main--flex">
-                            <div>
-                                <h3 className="course--detail--title">Course</h3>
-                                <h4 className="course--name">{course.title}</h4>
-                                <p>By {user && `${user.firstName} ${user.lastName}`}</p>
-                                <p>{course.description}</p>
-                            </div>
-                            <div>
-                                <h3 className="course--detail--title">Estimated Time</h3>
-                                <p>{course.estimatedTime}</p>
-
-                                <h3 className="course--detail--title">Materials Needed</h3>
-                                <ul className="course--detail--list">
-                                    {materials.map((material, index) => (
-                                     <li key={index}>{material}</li>
-                                ))}
-                            </ul>
-                            </div>
+                <main>
+                    <div className="actions--bar">
+                        <div className="wrap">
+                            {
+                                authenticatedUser && authenticatedUser.id === user.id && (
+                                    <>
+                                        <Link className="button" to={`/courses/${course.id}/update`}>Update Course</Link>
+                                        <button className="button" onClick={handleDelete}>Delete Course</button>
+                                    </>
+                                )
+                            }
+                            <Link className="button button-secondary" to="/courses">Return to List</Link>
                         </div>
-                    </form>
-                </div>
-            </main>
+                    </div>
+
+                    <div className="wrap">
+                        <h2>Course Detail</h2>
+                        <form>
+                            <div className="main--flex">
+                                <div>
+                                    <h3 className="course--detail--title">Course</h3>
+                                    <h4 className="course--name">{course.title}</h4>
+                                    <p>By {user && `${user.firstName} ${user.lastName}`}</p>
+                                    <ReactMarkdown>{course.description}</ReactMarkdown>
+                                </div>
+                                <div>
+                                    <h3 className="course--detail--title">Estimated Time</h3>
+                                    <p>{course.estimatedTime}</p>
+
+                                    <div>
+    <h3 className="course--detail--title">Materials Needed</h3>
+    <ul className="course--detail--list">
+        <ReactMarkdown>{course.materialsNeeded}</ReactMarkdown>
+    </ul>
+</div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </main>
             </div>
         </>
     );
