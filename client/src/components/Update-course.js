fc7bf7a8f4ca4c6bd5f84dec31bc851106f44e6a
Parselinho <parselinho@gmail.com>
2023-05-31 22:03:52 +0300

    Update Update-course.js

--- client/src/components/Update-course.js
+++ client/src/components/Update-course.js
@@ -14,7 +14,6 @@ const UpdateCourse = () => {
   const navigate = useNavigate();
   const { id } = useParams();
 
-  // Fetch the course data from the API
   useEffect(() => {
     const fetchCourse = async () => {
         try {
@@ -50,26 +49,18 @@ const UpdateCourse = () => {
     fetchCourse();
   }, [id, navigate, authenticatedUser]);
 
-  
-
-  // Handle form submission
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    let errorMessages = [];
-    if (title === '') errorMessages.push("Please provide a value for 'Title'");
-    if (description === '') errorMessages.push("Please provide a value for 'Description'");
-    setErrors(errorMessages);
-
-    if (errorMessages.length > 0) return;
+    const course = {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded
+    };
 
     try {
-      const response = await axios.put(`http://localhost:5000/api/courses/${id}`, {
-        title,
-        description,
-        estimatedTime,
-        materialsNeeded
-      }, {
+      const response = await axios.put(`http://localhost:5000/api/courses/${id}`, course, {
         headers: {
           'Authorization': `Basic ${btoa(`${authenticatedUser.emailAddress}:${authenticatedUser.password}`)}`
         }
@@ -77,26 +68,22 @@ const UpdateCourse = () => {
 
       if (response.status === 204) {
         navigate('/courses');
+      } else if (response.data && response.data.errors) {
+        setErrors(response.data.errors);
       }
     } catch (error) {
       console.error("Error updating course", error);
-      if (error.response) {
-        if (error.response.status === 500) {
-          navigate('/error'); // Redirect to error route on 500 status code
-        } else if (error.response.data.errors) {
-          setErrors(error.response.data.errors);
-        }
+      if (error.response && error.response.status === 500) {
+        navigate('/error');
       }
     }
   };
   
-  // Handle cancel button click
   const handleCancel = (event) => {
     event.preventDefault();
     navigate('/courses');
   };
 
-  // Render the UpdateCourse component
   return (
     <div>
       <h1 className='bold'>Update Course</h1>
