bb5372345829e7b93cb091c796fe7263c6b51ca0
Parselinho <parselinho@gmail.com>
2023-05-31 22:03:55 +0300

    Update Sign-up.js

--- client/src/components/Sign-up.js
+++ client/src/components/Sign-up.js
@@ -4,40 +4,30 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import { AuthContext } from './context/AuthContext';
 
-// UserSignUp component
 const UserSignUp = () => { 
-    // React Hook Form
     const { register, handleSubmit, formState: { errors }, setError } = useForm();
     const navigate = useNavigate();
-
-    // Access signIn function from AuthContext
     const { signIn } = useContext(AuthContext); 
 
-    // Handle form submission
     const onSubmit = async (data) => {
         try {
-            // Create new user
-            const response = await axios.post('http://localhost:5000/api/users', data);
-      
+            const response = await axios.post('http://localhost:5000/api/users', data); // Send POST request to create a new user
             if (response.status === 201) {
-                // Sign in newly created user
-                const signedIn = await signIn(data.emailAddress, data.password);
-        
+                const signedIn = await signIn(data.emailAddress, data.password); // Sign in the new user
                 if (signedIn) {
-                    navigate('/courses'); // Redirect to courses list on successful sign up
+                    navigate('/courses');
                 }
             }
         } catch (error) {
             console.error("Error creating user", error);
             if (error.response) {
                 if (error.response.status === 500) {
-                    navigate('/error'); // Redirect to error route on 500 status code
+                    navigate('/error'); 
                 } else if (error.response.data.errors) {
-                    // Set errors in the form
-                    error.response.data.errors.forEach(error => {
-                        setError(error.param, {
-                            type: "manual",
-                            message: error.msg
+                    error.response.data.errors.forEach(error => { // Loop through validation errors and set error messages
+                        setError(error.param, { // Set the error for each field
+                            type: "manual", // Manually set the error message
+                            message: error.msg // Set the error message to the message returned from the API
                         });
                     });
                 }
@@ -45,13 +35,11 @@ const UserSignUp = () => {
         }
     };
 
-    // Handle cancellation
     const handleCancel = (event) => {
         event.preventDefault();
-        navigate('/courses'); // Redirect to courses list on cancellation
+        navigate('/courses'); 
     };
 
-    // Render the UserSignUp component
     return (
         <>
             <div className="form--centered">
@@ -59,19 +47,19 @@ const UserSignUp = () => {
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <label htmlFor="firstName">First Name</label>
                     <input {...register("firstName", { required: true })} id="firstName" name="firstName" type="text" />
-                    {errors.firstName && <span>This field is required</span>}
+                    {errors.firstName && <span>{errors.firstName.message}</span>} 
 
                     <label htmlFor="lastName">Last Name</label>
                     <input {...register("lastName", { required: true })} id="lastName" name="lastName" type="text" />
-                    {errors.lastName && <span>This field is required</span>}
+                    {errors.lastName && <span>{errors.lastName.message}</span>}
 
                     <label htmlFor="emailAddress">Email Address</label>
                     <input {...register("emailAddress", { required: true })} id="emailAddress" name="emailAddress" type="email" />
-                    {errors.emailAddress && <span>This field is required</span>}
+                    {errors.emailAddress && <span>{errors.emailAddress.message}</span>}
 
                     <label htmlFor="password">Password</label>
                     <input {...register("password", { required: true })} id="password" name="password" type="password" />
-                    {errors.password && <span>This field is required</span>}
+                    {errors.password && <span>{errors.password.message}</span>}
 
                     <button className="button" type="submit">Sign Up</button>
                     <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
