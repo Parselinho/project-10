8ce2465cb4c75e0bae96988a42d4505dab7f092a
Parselinho <parselinho@gmail.com>
2023-05-31 12:37:22 +0300

    Update PrivateRoute.js

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -5,33 +5,20 @@ import { AuthContext } from './context/AuthContext';
 // PrivateRoute component
 const PrivateRoute = ({ children }) => {
   // Access authenticatedUser and setLastVisitedPage from AuthContext
-  const { authenticatedUser, setLastVisitedPage, checkPermissions, permissionsChecked } = useContext(AuthContext);
+  const { authenticatedUser, setLastVisitedPage } = useContext(AuthContext);
   const navigate = useNavigate();
-  const location = useLocation(); // useLocation is a hook provided by react-router-dom to get the current location object
+  const location = useLocation(); 
 
-  // Check if user is authenticated and permissions on component mount
+  // Check if user is authenticated on component mount
   useEffect(() => {
-      console.log(authenticatedUser);
-      if (!authenticatedUser) {
-      if (location.pathname !== '/forbidden' && location.pathname !== '/notfound') {
-        setLastVisitedPage(location.pathname);
-      }
-      navigate('/signin', { state: { from: location.pathname } });
-    } else if(location.pathname.includes('/courses/') && location.pathname.includes('/update')) {
-      checkPermissions(location.pathname);
+    if (!authenticatedUser) {
+            setLastVisitedPage(location.pathname);
+        navigate('/signin', { state: { from: location.pathname } });
     }
-  }, [authenticatedUser, navigate, setLastVisitedPage, location, checkPermissions]);
+  }, [authenticatedUser, navigate, setLastVisitedPage, location]);
 
-  useEffect(() => {
-    if (permissionsChecked === 'forbidden') {
-      navigate('/forbidden');
-    } else if (permissionsChecked === 'notfound') {
-      navigate('/notfound');
-    }
-  }, [navigate, permissionsChecked]);
-
-  // Render the children components if user is authenticated and permissions are checked, otherwise render null
-  return (authenticatedUser && (permissionsChecked === 'authorized' || !location.pathname.includes('/courses/') || !location.pathname.includes('/update'))) ? children : null;
+  // Render the children components if user is authenticated, otherwise render null
+  return authenticatedUser ? children : null;
 };
 
 export default PrivateRoute;
