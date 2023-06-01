b2620b01c8efbe3d382b1d077ad329cff12877af
Parselinho <parselinho@gmail.com>
2023-06-01 14:20:18 +0300

    Update SignOut.js

--- client/src/components/SignOut.js
+++ client/src/components/SignOut.js
@@ -8,8 +8,8 @@ const UserSignOut = ({ onSignOut }) => {
     // Perform sign out and redirect to courses list
     useEffect(() => {
         onSignOut(); // Call the onSignOut function passed as a prop
-        navigate('/courses'); // Redirect to courses list
-    }, [onSignOut, navigate]);
+        navigate('/courses'); 
+    }, [onSignOut, navigate]); // Call the effect when the component mounts and when the onSignOut function changes
 
     // Render nothing (null) since this component is only used for side effects
     return null;
