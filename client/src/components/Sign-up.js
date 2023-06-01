c35d6b8a7836f49685fc218d1cb39cd027ed8c6b
Parselinho <parselinho@gmail.com>
2023-06-01 23:15:40 +0300

    Update Sign-up.js

--- client/src/components/Sign-up.js
+++ client/src/components/Sign-up.js
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import { AuthContext } from './context/AuthContext';
 
 const UserSignUp = () => { 
-    const { register, handleSubmit, formState: { errors: formErrors } } = useForm(); 
+    const { register, handleSubmit } = useForm(); 
     const navigate = useNavigate();
     const { signIn } = useContext(AuthContext);
     const [errors, setErrors] = useState([]); 
@@ -42,35 +42,27 @@ const UserSignUp = () => {
         <>
             <div className="form--centered">
                 <h2 className='bold'>Sign Up</h2>
+                {/* Render validation errors, if any */}
                 {errors.length > 0 && (
-                    <div>
-                        <h2>Validation Errors</h2>
-                        <ul className='marginBottom'>
-                            {errors.map((error, index) => <li key={index}>{error}</li>)}
-                        </ul>
-                    </div>
-                )}
-                {Object.keys(formErrors).length > 0 && (
-                    <div>
-                        <h2>Form Validation Errors</h2>
-                        <ul className='marginBottom'>
-                            {Object.entries(formErrors).map(([field, error]) => 
-                              <li key={field}>{field}: {error.message}</li>)}
-                        </ul>
-                    </div>
+                  <div className="validation--errors">
+                    <h3>Validation Errors</h3>
+                    <ul>
+                        {errors.map((error, i) => <li key={i}>{error}</li>)}
+                    </ul>
+                  </div>
                 )}
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <label htmlFor="firstName">First Name</label>
-                    <input {...register("firstName", { required: "First name is required" })} id="firstName" name="firstName" type="text" />
+                    <input {...register("firstName")} id="firstName" name="firstName" type="text" />
 
                     <label htmlFor="lastName">Last Name</label>
-                    <input {...register("lastName", { required: "Last name is required" })} id="lastName" name="lastName" type="text" />
+                    <input {...register("lastName")} id="lastName" name="lastName" type="text" />
 
                     <label htmlFor="emailAddress">Email Address</label>
-                    <input {...register("emailAddress", { required: "Email address is required" })} id="emailAddress" name="emailAddress" type="email" />
+                    <input {...register("emailAddress")} id="emailAddress" name="emailAddress" type="email" />
 
                     <label htmlFor="password">Password</label>
-                    <input {...register("password", { required: "Password is required" })} id="password" name="password" type="password" />
+                    <input {...register("password")} id="password" name="password" type="password" />
 
                     <button className="button" type="submit">Sign Up</button>
                     <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
